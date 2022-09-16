c19254c98c91bc131a7f7e904532283f5b96ff78
Shaun Hubbard <shaunhubbard2013@icloud.com>
2022-09-16 18:29:39 -0400

    prod secrets added

--- infra/go.mod
+++ infra/go.mod
@@ -5,4 +5,8 @@ go 1.18
 require (
 	github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src v0.0.0-20220907123058-81050ae85a2c
 	github.com/aws/aws-lambda-go v1.34.1
+	github.com/aws/aws-sdk-go v1.44.100
+	github.com/aws/aws-secretsmanager-caching-go v1.1.0
 )
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- infra/main.go
+++ infra/main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	rtctokenbuilder "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/RtcTokenBuilder"
 	rtmtokenbuilder "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/RtmTokenBuilder"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
 	"log"
 	"math/rand"
-	"os"
 	"strconv"
 	"time"
 )
+
+var (
+	secretCache, _ = secretcache.New()
+)
+
+func GetAWSSecret(secretId string) (string, error) {
+	result, err := secretCache.GetSecretString("Meta_Pen_Test_Prod_Secrets")
+	if err != nil {
+		return "", errors.New("error: Failed to load secret from AWS Secrets Manager")
+	}
+	secretMap := map[string]string{}
+	json.Unmarshal([]byte(result), &secretMap)
+
+	return secretMap[secretId], nil
+}
 
 type Token struct {
 	UID    uint32 `json:"uid"`
@@ -21,9 +37,15 @@ type Token struct {
 	RTM    string `json:"rtm"`
 }
 
-func generateRTMToken(uid string) string {
-	appID := os.Getenv("APP_ID")
-	appCertificate := os.Getenv("CERTIFICATE")
+func generateRTMToken(uid string) (string, error) {
+	appID, idErr := GetAWSSecret("APP_IDs")
+	if idErr != nil {
+		return "", idErr
+	}
+	appCertificate, certErr := GetAWSSecret("CERTIFICATE")
+	if certErr != nil {
+		return "", certErr
+	}
 	expireTimeInSeconds := uint32(24 * 60 * 60)
 	currentTimestamp := uint32(time.Now().UTC().Unix())
 	expireTimestamp := currentTimestamp + expireTimeInSeconds
@@ -33,18 +55,24 @@ func generateRTMToken(uid string) string {
 	if err != nil {
 		log.Printf("Error %+v", err)
 	}
-	return result
+	return result, nil
 }
 
-func generateRtcToken(uid uint32, channelName string, role rtctokenbuilder.Role) string {
-	appID := os.Getenv("APP_ID")
-	appCertificate := os.Getenv("CERTIFICATE")
+func generateRtcToken(uid uint32, channelName string, role rtctokenbuilder.Role) (string, error) {
+	appID, idErr := GetAWSSecret("APP_IDs")
+	if idErr != nil {
+		return "", idErr
+	}
+	appCertificate, certErr := GetAWSSecret("CERTIFICATE")
+	if certErr != nil {
+		return "", certErr
+	}
 	tokenExpireTimeInSeconds := uint32(60 * 60 * 24)
 	result, err := rtctokenbuilder.BuildTokenWithUID(appID, appCertificate, channelName, uid, role, tokenExpireTimeInSeconds)
 	if err != nil {
 		log.Printf("Error %+v", err)
 	}
-	return result
+	return result, nil
 }
 
 func generateARandomUID() uint32 {
@@ -58,9 +86,21 @@ func getToken(event events.APIGatewayProxyRequest) (events.APIGatewayProxyRespon
 	rtcChannel := event.QueryStringParameters["Channel"]
 
 	rtmUid := strconv.FormatUint(uint64(uid), 10)
-	rtmToken := generateRTMToken(rtmUid)
+	rtmToken, rtmErr := generateRTMToken(rtmUid)
+	if rtmErr != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       rtmErr.Error(),
+			StatusCode: 500,
+		}, nil
+	}
 
-	rtcToken := generateRtcToken(uid, rtcChannel, rtctokenbuilder.RolePublisher)
+	rtcToken, rtcErr := generateRtcToken(uid, rtcChannel, rtctokenbuilder.RolePublisher)
+	if rtmErr != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       rtcErr.Error(),
+			StatusCode: 500,
+		}, nil
+	}
 
 	token := Token{
 		UID: uid, RTC: rtcToken, RTM: rtmToken, RTMUID: rtmUid,
